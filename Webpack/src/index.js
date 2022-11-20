a4d27ec45bb91c5b01dd0bc0ecc792320c1db57d
Victor Hugo <torugoestudos@gmail.com>
2022-11-20 01:27:44 -0300

    exercicio webpack

---
+++ Webpack/src/index.js
@@ -0,0 +1,27 @@
+import './styles.scss'
+import img from './background.jpg'
+
+function rootStyle(){
+  const elemRoot = document.getElementById('root')
+  elemRoot.classList.add('container')
+}
+
+function titleComponent(){
+  
+  const elemH1 = document.createElement('h1')
+  elemH1.innerHTML = 'Memphis Style'
+  elemH1.classList.add('title')
+  return elemH1
+
+}
+
+function imageComponent(){
+  const elemImg = new Image(1200, 800)
+  elemImg.src = img
+
+  return elemImg
+}
+
+rootStyle()
+document.body.appendChild(titleComponent())
+document.body.appendChild(imageComponent())
