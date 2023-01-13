fb174c69eeef2c59841b0f2a3017474f429ce10b
Victor Hugo <torugoestudos@gmail.com>
2023-01-13 11:51:54 -0300

    reset

--- Style Guide/src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('teste titulo', () => {
-  render(<App />);
-  const testCase = document.querySelector('.App h1');
-  expect(testCase).toHaveTextContent(/meu painel de controle/i);
-});
